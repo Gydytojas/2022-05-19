d3beaaa2911df3cdb95be5c84126f6248838dfe7
Gydytojas <savinis.vidas@gmail.com>
2022-05-23 18:00:25 +0300

    Sukurti 6 puslapiai ir keliai

--- index.js
+++ index.js
@@ -21,5 +21,17 @@ app.get('/trecia_uzduotis', (req, res) => {
     res.sendFile(__dirname + '/uzduotys/3.html')
 })
 
+app.get('/ketvirta_uzduotis', (req, res) => {
+    res.sendFile(__dirname + '/uzduotys/4.html')
+})
+
+app.get('/penkta_uzduotis', (req, res) => {
+    res.sendFile(__dirname + '/uzduotys/5.html')
+})
+
+app.get('/sesta_uzduotis', (req, res) => {
+    res.sendFile(__dirname + '/uzduotys/6.html')
+})
+
 app.listen(3000);
 
